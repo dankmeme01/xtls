0812a7e2d14bda8b7d702c0882448632bf57d496
dankmeme01 <42031238+dankmeme01@users.noreply.github.com>
2026-03-11 00:42:57 +0100

    aa

--- examples/tls_socket.cpp
+++ examples/tls_socket.cpp
@@ -1,20 +1,40 @@
 #include <xtls/xtls.hpp>
+#include <qsox/Resolver.hpp>
 #include <print>
 
-// Tries to connect to a TLS server running on localhost:4433, using plain bsd sockets
+// Resolves www.google.com and makes a HTTP/1.1 HTTPS request using TlsSocket
 
 int main() {
     auto& backend = xtls::OpenSSLBackend::get();
     auto context = backend.createContext(xtls::ContextType::Client).unwrap();
     context->setCertVerification(false).unwrap();
 
-    auto res = xtls::TlsSocket::connect(qsox::SocketAddress::parse("127.0.0.1:4433").unwrap(), context, "localhost");
+    std::println("Resolving www.google.com...");
+    auto addrs = qsox::resolver::resolve("www.google.com").unwrap();
+    std::println("Resolved address: {}", addrs.toString());
+
+    auto res = xtls::TlsSocket::connect(qsox::SocketAddress{addrs, 443}, context, "www.google.com");
     if (!res) {
         std::println("Failed to connect: {}", res.unwrapErr().message);
         return 1;
     }
 
     std::println("Handshake successful!");
+    auto socket = std::move(res).unwrap();
+    std::string_view req = "GET / HTTP/1.1\r\nHost: www.google.com\r\nConnection: close\r\n\r\n";
+    socket.send(req.data(), req.size()).unwrap();
+    uint8_t buf[4096];
+
+    while (true) {
+        auto res = socket.receive(buf, sizeof(buf));
+        if (!res) {
+            std::println("Receive error: {}", res.unwrapErr().message);
+            break;
+        }
+        size_t recvd = res.unwrap();
+        std::string_view data{reinterpret_cast<char*>(buf), recvd};
+        std::print("{}", data);
+    }
 
     return 0;
 }
--- include/xtls/TlsSocket.hpp
+++ include/xtls/TlsSocket.hpp
@@ -7,6 +7,18 @@
 
 namespace xtls {
 
+template <typename T>
+inline TlsResult<T> mapResult(qsox::NetResult<T>&& res) {
+    if (res.isOk()) {
+        if constexpr (std::is_void_v<T>) {
+            return geode::Ok();
+        } else {
+            return geode::Ok(std::move(res).unwrap());
+        }
+    }
+    return geode::Err(TlsError::custom(res.unwrapErr().message()));
+}
+
 class TlsSocket {
 public:
     TlsSocket(qsox::TcpStream stream, std::shared_ptr<Session> session);
@@ -18,9 +30,57 @@ public:
     static TlsResult<TlsSocket> connect(qsox::SocketAddress address, std::shared_ptr<Context> context, const std::string& hostname);
     static TlsResult<TlsSocket> connect(qsox::SocketAddress address, std::shared_ptr<Session> session);
 
-private:
+    /// Sends data over this socket, returning the number of bytes sent.
+    TlsResult<size_t> send(const void* buf, size_t size);
+    /// Receives data from this socket, returning the number of bytes received.
+    TlsResult<size_t> receive(void* buf, size_t size);
+
+    qsox::NetResult<qsox::SocketAddress> localAddress() const;
+    qsox::NetResult<qsox::SocketAddress> remoteAddress() const;
+
+    /// Get the handle to the inner `qsox::TcpStream`.
+    inline qsox::TcpStream& inner() noexcept {
+        return m_stream;
+    }
+
+protected:
     qsox::TcpStream m_stream;
     std::shared_ptr<Session> m_session;
+
+    template <typename T = void>
+    TlsResult<T> performAction(auto&& fn) requires (std::is_invocable_r_v<TlsResult<T>, decltype(fn)>) {
+        while (true) {
+            auto res = fn();
+
+            // always try to poll writes
+            GEODE_UNWRAP_INTO(auto pair, m_session->getEncryptedData());
+            auto [edata, esize] = pair;
+            while (esize > 0) {
+                GEODE_UNWRAP(mapResult(m_stream.sendAll(edata, esize)));
+                GEODE_UNWRAP(m_session->notifyEncryptedSent(static_cast<size_t>(esize)));
+            }
+
+            if (res.isOk()) {
+                if constexpr (std::is_void_v<T>) {
+                    return geode::Ok();
+                } else {
+                    return geode::Ok(std::move(res).unwrap());
+                }
+            }
+
+            auto err = std::move(res).unwrapErr();
+            if (err == TlsError::WANT_READ) {
+                uint8_t buf[4096];
+                GEODE_UNWRAP_INTO(auto recvd, mapResult(m_stream.receive(buf, sizeof(buf))));
+                GEODE_UNWRAP(m_session->feedEncryptedData(buf, static_cast<size_t>(recvd)));
+            } else if (err == TlsError::WANT_WRITE) {
+                // nothing to do here
+                continue;
+            } else {
+                return geode::Err(std::move(err));
+            }
+        }
+    }
 };
 
 }
