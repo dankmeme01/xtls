bb194dee053e1044bda7cfcc5307814d507904bb
dankmeme01 <42031238+dankmeme01@users.noreply.github.com>
2026-03-11 01:56:41 +0100

    head

--- examples/tls_socket.cpp
+++ examples/tls_socket.cpp
@@ -7,7 +7,7 @@
 int main() {
     auto& backend = xtls::OpenSSLBackend::get();
     auto context = backend.createContext(xtls::ContextType::Client).unwrap();
-    context->setCertVerification(false).unwrap();
+    context->setCertVerification(false).unwrap(); // don't do this in production :)
 
     std::println("Resolving www.google.com...");
     auto addrs = qsox::resolver::resolve("www.google.com").unwrap();
@@ -21,7 +21,7 @@ int main() {
 
     std::println("Handshake successful!");
     auto socket = std::move(res).unwrap();
-    std::string_view req = "GET / HTTP/1.1\r\nHost: www.google.com\r\nConnection: close\r\n\r\n";
+    std::string_view req = "HEAD / HTTP/1.1\r\nHost: www.google.com\r\nConnection: close\r\n\r\n";
     socket.send(req.data(), req.size()).unwrap();
     uint8_t buf[4096];
 
